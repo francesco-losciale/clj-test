29c87a5dadcf92da7c68a33607be5af1cb959d04
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-16 10:58:30 +0000

    added exercise solutions up to p. 179 of "Living Clojure"

--- src/clj_test/library.clj
+++ src/clj_test/library.clj
@@ -1,4 +1,24 @@
 (ns clj-test.library)
 
+(def arr '(1 2 3 4))
+
+(defn penultimate [s]
+  (nth (reverse s) 1))
+
+(defn sum-all-seq [s]
+  (reduce + s))
+
+(defn filter-even [s]
+  (filter even? s))
+
+(defn palindrome [s]
+  (= s (if (= (class s) java.lang.String)
+         (clojure.string/reverse s)
+         (reverse s))))
+
+(defn dup-elem [s]
+  (apply concat (map (partial repeat 2) s)))
+
+
 (defn function-in-library [param1]
   (str "Hello" param1))
