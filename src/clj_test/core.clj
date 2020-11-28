1f3fecbb5b66d05c47e31f71925ccc31ec0b4d5f
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-11-28 12:09:06 +0000

    completed week 2 day 1 of exercises in "Living Clojure" pag. 182

--- src/clj_test/core.clj
+++ src/clj_test/core.clj
@@ -699,3 +699,4 @@ print-nums                                                  ; only when evaluate
 (= (factorial 5) 120)
 (= (factorial 8) 40320)
 
+(= [2 4] (let [[a b c d e] [0 1 2 3 4]] [c e]))
