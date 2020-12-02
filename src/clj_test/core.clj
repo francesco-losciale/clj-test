981b5f0847d56dd0804b429af928bc2b33b05d8b
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-12-02 22:15:15 +0000

    added more tests around is-btree?

--- src/clj_test/core.clj
+++ src/clj_test/core.clj
@@ -901,6 +901,7 @@ print-nums                                                  ; only when evaluate
 (is-btree? [1 1 [1 1]])
 (= (is-btree? [1 [2 [3 [4 nil nil] nil] nil] nil]) true)
 (= (is-btree? [1 [2 nil nil] [3 nil nil] [4 nil nil]]) false)
-
-(= (is-btree? '(:a (:b nil nil) nil))
-   true)
+(= (is-btree? [1 nil [2 [3 nil nil] [4 nil nil]]]) true)
+(= (is-btree? '(:a (:b nil nil) nil)) true)
+(= (is-btree? [1 [2 [3 [4 false nil] nil] nil] nil]) false)
+(= (is-btree? '(:a nil ())) false)
