f57d0a29cdf2c055ddec5c83d811b9659e2dabfc
Francesco Losciale <francesco.losciale@thoughtworks.com>
2020-12-05 18:40:41 +0000

    first version of is-symmetric?

--- src/clj_test/core.clj
+++ src/clj_test/core.clj
@@ -902,3 +902,23 @@ print-nums                                                  ; only when evaluate
 (= (is-btree? '(:a (:b nil nil) nil)) true)
 (= (is-btree? [1 [2 [3 [4 false nil] nil] nil] nil]) false)
 (= (is-btree? '(:a nil ())) false)
+
+
+(defn is-symmetric? [t]
+  (if (and (not (empty? t)) (is-btree? t))
+    (and (is-symmetric? (nth t 1)) (is-symmetric? (nth t 2)))
+    (nil? t)))
+(= (is-symmetric? '()) false)
+(= (is-symmetric? '(:a)) false)
+(= (is-symmetric? '(:a (:b nil nil) (:b nil nil))) true)
+(= (is-symmetric? '(:a (:b nil nil) nil)) false)
+(= (is-symmetric? '(:a (:b nil nil) (:c nil nil))) false)
+(= (is-symmetric? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+        [2 [3 nil [4 [6 nil nil] [5 nil nil]]] nil]])
+   true)
+(= (is-symmetric? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+        [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+   false)
+(= (is-symmetric? [1 [2 nil [3 [4 [5 nil nil] [6 nil nil]] nil]]
+        [2 [3 nil [4 [5 nil nil] [6 nil nil]]] nil]])
+   false)
